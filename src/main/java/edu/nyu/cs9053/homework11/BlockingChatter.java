f1ddd6be818422aaa8b3414461081f761e15500d
amanmahato <akm533@nyu.edu>
2017-06-08 18:14:38 -0400

    CharServer Updated Files

---
+++ src/main/java/edu/nyu/cs9053/homework11/BlockingChatter.java
@@ -0,0 +1,29 @@
+package edu.nyu.cs9053.homework11;
+
+import java.io.*;
+
+
+public class BlockingChatter implements Chatter {
+
+    private final PrintWriter out;
+    private final BufferedReader in;
+    private final BufferedReader stdIn;
+
+    public BlockingChatter(InputStream chatServerInput, OutputStream chatServerOutput, InputStream userInput) {
+        out=new PrintWriter(chatServerOutput,true);
+        in=new BufferedReader(new InputStreamReader(chatServerInput));
+        stdIn= new BufferedReader(new InputStreamReader(userInput));
+    }
+
+    @Override public void run() {
+        String userInput;
+        try {
+            while ((userInput = stdIn.readLine()) != null) {
+                out.println(userInput);
+                System.out.println(in.readLine());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/edu/nyu/cs9053/homework11/Chatter.java
@@ -0,0 +1,5 @@
+package edu.nyu.cs9053.homework11;
+
+
+public interface Chatter extends Runnable {
+}
